aeb94becef914c8f8f5801f95e3d36a23acdde95
Adam Furtenbach <adam.furtenbach@gmail.com>
2013-12-28 00:24:53 +0100

    nya bilder

--- pack.sh
+++ pack.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 rm -f image.animation_mode:none.zip
-zip image.animation_mode:none.zip *.js manifest.json logo*.png
+convert jam.xcf -flatten -transparent white -resize 128x128 logo128.png
+convert jam.xcf -flatten -transparent white -resize 64x64 logo64.png
+convert jam.xcf -flatten -transparent white -resize 32x32 logo32.png
+zip gif_jam.zip *.js manifest.json logo*.png
